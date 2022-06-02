6089f78272d4c0809c0aae9128adeff1eb423dd9
Rishab Surana <rishab111195@gmail.com>
2022-06-02 16:15:14 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  let a;
-  console.log(a);
-  return (
-
-
-    <div className="App">
-              <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> 1 save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn vue
-        </a>
-      </header>
-    </div>
-  );
+    let a;
+    console.log(a);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> 1 save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn react
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
